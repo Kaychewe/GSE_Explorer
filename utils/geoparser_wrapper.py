bde05fd013a63a14466be511f565862665c58029
Kaychewe <chewe.kasonde@outlook.com>
2025-06-14 15:42:55 -0400

    created virtual env gpw

---
+++ utils/geoparser_wrapper.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# file:         geoparser_wrapper.py
+# author:       Kasonde Chewe 
+# description:  A simple set of utility commands to use the NCBI geoparser module
+
+
+import os 
+import argparse
+import GEOparse
