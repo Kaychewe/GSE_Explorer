7c30cd41086275eec56c1b3b1910d089741c261e
Kaychewe <chewe.kasonde@outlook.com>
2025-06-14 16:37:41 -0400

    testing argument parsing

--- utils/gpw.py
+++ utils/gpw.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-# file:         geoparser_wrapper.py
+# file:         gpw.py (geo-parse-wrapper.py)
 # author:       Kasonde Chewe 
 # description:  A simple set of utility commands to use the NCBI geoparser module
-
 
 import os 
 import argparse
 import GEOparse
 import logging
+
+
